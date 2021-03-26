b76f8809f497f9e3f849470b1da5ed6e6c32856c
vinod699 <kosarajuvinod9999@gmail.com>
2021-03-26 17:58:37 +0530

    welcom to snake and ladder problem

---
+++ src/snakeandladder/snakeandladder.java
@@ -0,0 +1,11 @@
+package snakeandladder;
+
+public class snakeandladder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcometosnakeandladder");
+
+	}
+
+}
